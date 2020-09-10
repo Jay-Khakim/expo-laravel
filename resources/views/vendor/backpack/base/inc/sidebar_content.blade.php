93ab5355cac7f09e99fe71c0e6802093385ce985
Jay-Khakim <mgmediajay@gmail.com>
2020-09-10 16:52:32 +0500

    sidebar customizing

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,7 +1,7 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('category') }}'><i class='nav-icon la la-question'></i> Categories</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('membership') }}'><i class='nav-icon la la-question'></i> Memberships</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('movie') }}'><i class='nav-icon la la-question'></i> Movies</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('news') }}'><i class='nav-icon la la-question'></i> News</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('picture') }}'><i class='nav-icon la la-question'></i> Pictures</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('category') }}'><i class="nav-icon la la-bars"></i></i> Categories</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('membership') }}'><i class='nav-icon la la-users'></i> Memberships</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('movie') }}'><i class='nav-icon la la-youtube'></i> Movies</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('news') }}'><i class='nav-icon la la-blog'></i> News</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('picture') }}'><i class='nav-icon la la-image'></i> Pictures</a></li>
