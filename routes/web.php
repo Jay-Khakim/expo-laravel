8779dee7f0d9abf0a647d580b90d79cd03636034
Jay-Khakim <mgmediajay@gmail.com>
2020-09-10 12:12:47 +0500

    localisation done

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,15 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::redirect('/', '/en');
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => '{language}'], function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    // Auth::routes();
+
+    Route::get('/home', 'HomeController@index')->name('home');
 });
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
