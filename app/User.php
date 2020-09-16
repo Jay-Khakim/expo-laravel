389f48d4185ce5e402661d3dd17ae152d22850fd
Jay-Khakim <mgmediajay@gmail.com>
2020-09-16 11:49:20 +0500

    Permission manager installing

--- app/User.php
+++ app/User.php
@@ -5,10 +5,14 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Backpack\CRUD\app\Models\Traits\CrudTrait; // <------------------------------- this one
+use Spatie\Permission\Traits\HasRoles;// <---------------------- and this one
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use CrudTrait; // <----- this
+    use HasRoles; // <------ and this
 
     /**
      * The attributes that are mass assignable.
